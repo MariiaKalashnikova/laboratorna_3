4cef99f29e204660d6f2897a7b54019a82e04b5f
MariiaKalashnikova <122563884+MariiaKalashnikova@users.noreply.github.com>
2025-04-06 10:46:50 +0300

    Delete laboratorna3_3 directory lab3-4_3

--- laboratorna3_3/service.py
+++
@@ -1,5 +0,0 @@
-def fill_column(wind_direction, wind_direction_N, wind_direction_S):
-    if wind_direction == wind_direction_N or wind_direction == wind_direction_S:  
-        return False
-    else:
-        return True
